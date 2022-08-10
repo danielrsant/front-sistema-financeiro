6a8a5e6f9eff6b0daab6bbcffc5c1791944fbcb7
danielrsant <danrsant14@gmail.com>
2022-08-09 22:53:28 -0300

    environment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  // API: 'https://a0cdbb849ee8.ngrok.io'
-  API: 'http://localhost:3000'
+  API: 'https://puc-projeto-integrado.herokuapp.com'
+  // API: 'http://localhost:3000'
 };
